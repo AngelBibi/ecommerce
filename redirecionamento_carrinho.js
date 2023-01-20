73d72694161473fc69938f762914b7e23ebe5e93
raquelmattos1 <93119361+raquelmattos1@users.noreply.github.com>
2023-01-20 13:34:46 +0000

    Add files via upload

---
+++ redirecionamento_carrinho.js
@@ -0,0 +1,11 @@
+sessionStorage.descricao = document.getElementById('descricao').innerText;
+sessionStorage.valor = document.getElementById('valor').innerText;
+
+function resumoCompra(){
+    window.location = "carrinho.html"
+}sessionStorage.descricao = document.getElementById('descricao').innerText;
+sessionStorage.valor = document.getElementById('valor').innerText;
+
+function resumoCompra(){
+    window.location = "carrinho.html"
+}
